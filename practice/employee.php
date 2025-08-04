215af7228b0f235eb51a598ed7efab3000f03d46
FaisalBss <faisalbsb@hotmail.com>
2025-08-04 14:10:51 +0300

    Update employee.php

--- practice/employee.php
+++ practice/employee.php
@@ -6,19 +6,21 @@ class Employee {
     public $name;
     public $address;
     public $age;
-    public $tax;
     public $salary;
+    public $tax;
 
+
+    public function __construct($name, $age, $address, $salary, $tax){
+
+        $this->name = $name;
+        $this->age = $age;
+        $this->address = $address;
+        $this->salary = $salary;
+        $this->tax = $tax;
+    }
 
     public function calculateSalary() {
         return $this->salary - ($this->salary * $this->tax / 100);
     }
-    // public function __construct($name, $age, $tax, $salary){
-
-    //     $this->name = $name;
-    //     $this->age = $age;
-    //     $this->salary = $salary;
-    //     $this->tax = $tax;
-    // }
-
+    
 }
